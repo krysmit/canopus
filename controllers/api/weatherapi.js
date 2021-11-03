071081fb11838e3ed70aff8352054226373d4ff9
Krystyn Smith <krskrsmith@gmail.com>
2021-11-02 23:10:08 -0500

    weather api code complete for now. added a rough code for the clearsky page

--- controllers/api/weatherapi.js
+++ controllers/api/weatherapi.js
@@ -3,8 +3,8 @@ var apiKey = "ad4e109e5b7a1f3554d123fbf819c27f";
 //calling API for forecast
 function searchButtonHandler(searchInput) {
     console.log("forecast search input: ", searchInput);
-    var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid=" + apiKey+"&units=imperial";
-    var weatherURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=" + apiKey + "&units=imperial";
+   //need to fill in the city name search syntax
+    var weatherURL = "api.openweathermap.org/data/2.5/weather?q=" + {city} + "&appid=" + apiKey + "&units=imperial";
     console.log("forecast api url: ", weatherURL)
     $.ajax({
         //get data from api
@@ -15,22 +15,8 @@ function searchButtonHandler(searchInput) {
             // var appendedDates = [];
             var appendHTML = 0;
 
-            for (let i=0; i < data.list.length; i=i+8) {
-                var curr = data.list[i]
-                var currDateText = curr.dt_txt
-                var currDate = currDateText.split(" ")[0];
-                appendHTML += `<div class="card btn-primary">
-                <h2>${currDate}</h2>
-                <h3>Temp:${curr.main.temp}
-                <img src="https://openweathermap.org/img/wn/${curr.weather[0].icon}@2x.png" /></h3>
-                <h4>Description: ${curr.weather[0].description}</h4>
-                <h5>Humidity: ${curr.main.humidity}</h5>
-                <h6>Wind Speed: ${curr.wind.speed}</h6>
-
-                                `
-
-                console.log("current date: ", currDate)
-            }
+            // <div class="card btn-primary">
+            //     <h2>Temp:${weather.main}</h2>
 
             // $("#fiveday").html(appendHTML)
         
