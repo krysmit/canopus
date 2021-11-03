3e015d9b4dd57453dda9f1e1547e3ee2f57652e9
Krystyn Smith <krskrsmith@gmail.com>
2021-11-03 00:27:35 -0500

    updated syntax from the name, description, and funding to city and state

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,13 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const city = document.querySelector('#project-name').value.trim();
+  const state = document.querySelector('#project-desc').value.trim();
 
-  if (name && needed_funding && description) {
+  if (city && state) {
     const response = await fetch(`/api/projects`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ city, state }),
       headers: {
         'Content-Type': 'application/json',
       },
