b33e1fcd9341896ccc85b67932f8622935abe89b
Krystyn Smith <krskrsmith@gmail.com>
2021-11-03 23:02:40 -0500

    adjusting code to retrieve weather properly

--- public/js/apis/weatherapi.js
+++ public/js/apis/weatherapi.js
@@ -1,48 +1,35 @@
 var apiKey = "ad4e109e5b7a1f3554d123fbf819c27f";
+var weatherURL = "api.openweathermap.org/data/2.5/weather?q=" + {city} + "&appid=" + apiKey + "&units=imperial";
 
-//calling API for forecast
-// function searchButtonHandler(searchInput) {
-//     console.log("forecast search input: ", searchInput);
-
-   //need to fill in the city name search syntax
-    var weatherURL = "api.openweathermap.org/data/2.5/weather?q=" + {city} + "&appid=" + apiKey + "&units=imperial";
+function currentWeather(searchInput) {
+    console.log("current weather search input: ", searchInput);
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + apiKey+"&units=imperial";
+     console.log("current weather api url: ", weatherURL)
     $.ajax({
         //get data from api
         url: weatherURL,
         method:"GET"
     }).then(function (data) {
-            console.log("forecast data: ", data);
-            // var appendedDates = [];
-            var appendHTML = 0;
+            console.log("current weather data: ",data);
 
-            // <div class="card btn-primary">
-            //     <h2>Temp:${weather.main}</h2>
+            var currentUV = `https://api.openweathermap.org/data/2.5/uvi?q=&lat=${lat}&lon=${lon}&appid=${key}`;
 
-            // $("#fiveday").html(appendHTML)
+            console.log("currentUV: ", currentUV);
+
+            var appendHTML = "";
+            
+                appendHTML += `<div class="card btn-primary">
+                <h2>${searchInput}</h2>
+                <h3>Temp:${data.main.temp}
+                <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h3>
+                <h4>Description: ${data.weather[0].description}</h4>
+                <h5>Humidity: ${data.main.humidity}</h5>
+                <h6>Wind Speed: ${data.wind.speed}</h6>
+                                `
+
+            $("#weatherNow").html(appendHTML)
         
+
     })   
-// }
+}
 
-
-
-
-// fetch('https://data.cityofchicago.org/resource/8pix-ypme.json?red=true')
-//             .then(function (response) {
-//                 return response.json();
-//             })
-//             .then(function (data) {
-//                 appendData(data);
-//             })
-//             .catch(function (err) {
-//                 console.log('error: ' + err);
-//             });
-//         function appendData(data) {
-//             var mainContainer = document.getElementById("nearInfo");
-//             for (var i = 0; i < 3; i++) {
-//                 var div = document.createElement("div");
-//                 div.innerHTML = 'Station: ' + data[i].station_descriptive_name + ' ' + data[i].stop_name;
-//                 mainContainer.appendChild(div);
-//             }
-//         }
-
-//         let map;
