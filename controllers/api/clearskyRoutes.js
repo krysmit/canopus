c4ecaf0b89d18a0c7361c6aea36a8d66d602c6a5
Krystyn Smith <krskrsmith@gmail.com>
2021-11-03 23:46:21 -0500

    adding more to the clearsky nav

--- controllers/api/clearskyRoutes.js
+++ controllers/api/clearskyRoutes.js
@@ -1,3 +1,23 @@
 const router = require('express').Router();
 const { Clearsky } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const projectData = await Project.create({
+          where: {
+            id: req.params.id,
+            user_id: req.session.user_id,
+          },
+        });
+    
+        if (!projectData) {
+          res.status(404).json({ message: 'No project found with this id!' });
+          return;
+        }
+    
+        res.status(200).json(projectData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+  });
