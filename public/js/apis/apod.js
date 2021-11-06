d50b4d324d40374aaf747adb415ac6fd8f67e7e9
Krystyn Smith <krskrsmith@gmail.com>
2021-11-05 21:33:58 -0500

    figured in the code for picture api. getting it up and running slowly though

--- public/js/apis/apod.js
+++ public/js/apis/apod.js
@@ -1,3 +1,31 @@
 var apiKey = "krc72TdKcUi6vydafrxqDc2GyfOJkUujJdIYyDzR";
+const fetch = require("node-fetch");
 
-var astroApi = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey;
+router.get("/fetch_image", async (req, res) => {
+    console.log("/fetch_image endpoint called");
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+    const options = {   
+        "method": "GET",
+    }
+
+    
+    const response = await fetch(url, options)
+    .then(res => res.json())
+    .catch(e => {
+        console.error({
+            "message": "no",
+            error: e,
+        });
+    });
+
+
+
+    console.log("RESPONSE ", response);
+    res.json(response.url);
+
+    const image = response.url;
+    return image;
+    
+
+
+});
