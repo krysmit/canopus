f1a8b0841a6201700a86285003ed86ceb8983aa7
Krystyn Smith <krskrsmith@gmail.com>
2021-11-10 13:14:39 -0600

    minor syntax changes to the code

--- public/js/apis/weather.js
+++ public/js/apis/weather.js
@@ -1,29 +1,62 @@
 // const fetch = require("node-fetch");
 const APIKEY = "d19d0c16157ce3486e364b59c5b2484e";
-var searchcity = document.getElementByID('searchcity').value;
 
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || []
 
-document.getElementByID('citystateBtn').addEventListener('click',search);
+$("#search-style").click(function (event){
+    event.preventDefault()
+    let searchInput = $("#searchInput").val();
+    console.log("local storage search input: ", searchInput);
+    var checkArray = searchHistory.includes(searchInput);
 
-console.log(">>>>>>weather api js page accessed<<<<<<");
-
-function search() {
-router.get("/fetch_data", async (req, res) => {
-    console.log("<<<<<<<inside the search function>>>>>>");
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchcity}&appid=${APIKEY}`;
-    const options = {   
-        "method": "GET",
-    }
+    if (checkArray === false) {
+        searchHistory.push(searchInput);
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+//adding searched items to a list
+    } else {
+        searchHistory.push(searchInput);
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
     
-    const response = await fetch(url, options)
-    .then(res => res.json())
-    .catch(e => {
-        console.error({
-            "message": "no",
-            error: e,
-        });
-    });
-    console.log(response);
-    res.json(response);
+        var historyButton = $("<a>").attr({
+          class: "list-group-item list-group-item-action",
+          href: "#",
+        })
+    
+    historyButton.text(searchInput);
+    $("#searchList").append(historyButton);
 
-})};
+    currentWeather(searchInput);
+    searchButtonHandler(searchInput);
+
+    }});
+
+
+
+
+
+// var searchcity = document.getElementByID('searchcity').value;
+
+// document.getElementByID('citystateBtn').addEventListener('click',search);
+
+// console.log(">>>>>>weather api js page accessed<<<<<<");
+
+// function search() {
+// router.get("/fetch_data", async (req, res) => {
+//     console.log("<<<<<<<inside the search function>>>>>>");
+//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchcity}&appid=${APIKEY}`;
+//     const options = {   
+//         "method": "GET",
+//     }
+    
+//     const response = await fetch(url, options)
+//     .then(res => res.json())
+//     .catch(e => {
+//         console.error({
+//             "message": "no",
+//             error: e,
+//         });
+//     });
+//     console.log(response);
+//     res.json(response);
+
+// })};
