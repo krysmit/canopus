2619f6efd6cf9fde0d6cb5c9b1457c451730da0a
Krystyn Smith <krskrsmith@gmail.com>
2021-11-12 16:19:48 -0600

    had to re-clone to my computer. not sure why it wouldn't work otherwise. put the weather API BACK to the front end... still getting errors

--- controllers/api/weather.js
+++ controllers/api/weather.js
@@ -1,25 +1,25 @@
-const router = require('express').Router();
-const fetch = require("node-fetch");
+// const router = require('express').Router();
+// const fetch = require("node-fetch");
 
-const APIKEY = "d19d0c16157ce3486e364b59c5b2484e";
+// const APIKEY = "d19d0c16157ce3486e364b59c5b2484e";
 
-router.get("/fetch_data", async (req, res) => {
-    console.log("/fetch_data endpoint called");
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${APIKEY}`;
-    const options = {   
-        "method": "GET",
-    }
+// router.get("/fetch_data", async (req, res) => {
+//     console.log("/fetch_data endpoint called");
+//     const url = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${APIKEY}`;
+//     const options = {   
+//         "method": "GET",
+//     }
 
-    const response = await fetch(url, options)
-    .then(res => res.json())
-    .catch(e => {
-        console.error({
-            "message": "no",
-            error: e,
-        });
-    });
-    console.log(response);
-    res.json(response);
+//     const response = await fetch(url, options)
+//     .then(res => res.json())
+//     .catch(e => {
+//         console.error({
+//             "message": "no",
+//             error: e,
+//         });
+//     });
+//     console.log(response);
+//     res.json(response);
 
-});
-module.exports = router;
+// });
+// module.exports = router;
--- public/js/apis/weather.js
+++ public/js/apis/weather.js
@@ -1,13 +1,13 @@
 var cityInput = document.getElementByID('cityInput').value;
 
-document.getElementByID('citystateBtn').addEventListener('click',search);
-
 console.log(">>>>>>weather api js page accessed<<<<<<");
+
+document.getElementByID("citystateBtn").addEventListener('click',search());
 
 function search() {
 router.get("/fetch_data", async (req, res) => {
     console.log("<<<<<<<inside the search function>>>>>>");
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${APIKEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=` + {cityInput} + `&appid=${APIKEY}`;
     const options = {   
         "method": "GET",
     }
