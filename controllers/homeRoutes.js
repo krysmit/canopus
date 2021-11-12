f641676629cc02c14af5cda2b8d17d791c424306
Krystyn <krskrsmith@gmail.com>
2021-11-12 11:21:32 -0600

    possibly found why dates wont post. testing after push

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
     const projects = projectData.map((project) => project.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
+    res.render('searchhistory', { 
       projects, 
       logged_in: req.session.logged_in 
     });
