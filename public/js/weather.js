8132faf06f8844ce84a3fedefc6712af799a22dd
Krystyn Smith <krskrsmith@gmail.com>
2021-11-12 20:05:13 -0600

    continuing changes to weather api. very close to pulling correctly.

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,4 +1,4 @@
-var APIKEY = "ad4e109e5b7a1f3554d123fbf819c27f";
+var APIKEY = "d19d0c16157ce3486e364b59c5b2484e";
 
 // var cityInput = document.getElementByID("cityInput").value;
 
@@ -7,14 +7,14 @@ var APIKEY = "ad4e109e5b7a1f3554d123fbf819c27f";
 // document.getElementByID("citystateBtn").addEventListener('click',search());
 
 // function search() {
-// router.get("/fetch_response", async (req, res) => {
+// router.get("/fetch_data", async (req, res) => {
 //     console.log("<<<<<<<inside the search function>>>>>>");
-//     const url = `https://api.openweathermap.org/response/2.5/weather?q=` + {cityInput} + `&appid=${APIKEY}`;
+//     const url = `https://api.openweathermap.org/data/2.5/weather?q=` + {cityInput} + `&appid=${APIKEY}`;
 //     const options = {   
 //         "method": "GET",
 //     }
     
-//     const response = await fetch(url, options)
+//     const data = await fetch(url, options)
 //     .then(res => res.json())
 //     .catch(e => {
 //         console.error({
@@ -22,8 +22,8 @@ var APIKEY = "ad4e109e5b7a1f3554d123fbf819c27f";
 //             error: e,
 //         });
 //     });
-//     console.log(response);
-//     res.json(response);
+//     console.log(data);
+//     res.json(data);
 
 // })};
 
@@ -31,9 +31,10 @@ $("#citystateBtn").on("click", function (e) {
     e.preventDefault();
     $("#tablebody").empty();  
 
-    const cityInput = $("#cityInput").value;
+    // const cityInput = $("#cityInput").value;
+    // console.log(cityInput);
 
-    const queryURL = `https://api.openweathermap.org/response/2.5/weather?q=${cityInput}&appid=${APIKEY}`;
+    const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${APIKEY}`;
 
 $.ajax({
   url: queryURL,
@@ -41,32 +42,31 @@ $.ajax({
     alert('Please enter a valid city name');
 }
 })
-.then(function (response) {
-  if (response.length === 0) {
+.then(function (data) {
+  if (data.length === 0) {
     let noCity = "Please enter a real place bish";
     $("#tablebody").empty();
     $("#tablebody").append(noCity);
     $(document).ready(function(){
     });
   }
-
-  const cityArray = [];
     // FOR any results, display as such:
-    for (let i = 0; i < response.length; i++) {
-      console.log(response)
-      cityArray.push(response[i]);
-      console.log(response)
+    else {
+      const cityArray = [];
+      console.log(data)
+      cityArray.push(data);
+      console.log(data)
 
       let makeCityOutput =
         `<span class="cityStuff">City: </span>` +
-        response[i].main +
+        data.main +
         "<br>" +
         `<span class="cityStuff">The conditions from </span>` +
-        response[i].description +
+        data.description +
         "<br><br>" +
         `<img style="display=center src="$" width="200" height="300">` +
         "<br><br><br>";
-        console.log(response)
+        console.log(data)
 
       let html = `<tr><td> ${makeCityOutput} </td></tr>`;
       $("#tablebody").append(html);
